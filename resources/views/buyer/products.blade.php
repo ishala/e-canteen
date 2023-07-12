7c865e5df1ce40892100ac2c9ebe9f13ca425775
ishala <faishalald22@gmail.com>
2023-07-13 03:13:18 +0700

    penyesuaian fungsi checkbox keranjang

--- resources/views/buyer/products.blade.php
+++ resources/views/buyer/products.blade.php
@@ -8,9 +8,11 @@
     <title>{{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ $style }}">
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+        
     <script>
         $(document).ready(function() {
             let category = $('#dataCategory').data('value');
